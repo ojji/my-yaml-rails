3e497da26e7a0b0a9dd04df477da639ba20fd7c4
Zoltán Tóth <zshun86@gmail.com>
2013-08-02 09:35:42 +0200

    Yaml jquery scripts added to installer generator.

--- lib/yaml/generator/install_generator.rb
+++ lib/yaml/generator/install_generator.rb
@@ -6,7 +6,14 @@ module Yaml
 			source_root File.join(File.dirname(__FILE__), "template")
 
 			def add_assets
-				#to-do
+				insert_into_file "app/assets/javascripts/application#{detect_js[0]}", "#{detect_js[1]} require jquery.tabs\n", :after => "jquery_ujs\n"
+				insert_into_file "app/assets/javascripts/application#{detect_js[0]}", "#{detect_js[1]} require jquery.syncheight\n", :after => "jquery.tabs\n"
+			end
+
+			def detect_js
+				return ['.coffee', '#='] if File.exists?('app/assets/javascripts/application.coffee')
+				return ['.js.coffee', '#='] if File.exists?('app/assets/javascripts/application.js.coffee')
+				return ['.js', '//='] if File.exists?('app/assets/javascripts/application.js')
 			end
 
 			def create_layout
