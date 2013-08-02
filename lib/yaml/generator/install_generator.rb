3dd85cf15c892396d05f1bdb8b78376117f9ef1a
Zoltán Tóth <zshun86@gmail.com>
2013-08-02 14:50:42 +0200

    Change require yaml_modules_and_settings in application.css.scss to @import.

--- lib/yaml/generator/install_generator.rb
+++ lib/yaml/generator/install_generator.rb
@@ -15,7 +15,7 @@ module Yaml
 				append_to_file "app/assets/stylesheets/yaml_modules_and_settings.scss", File.read(yaml_typography)
 				append_to_file "app/assets/stylesheets/yaml_modules_and_settings.scss", "\n@import 'yaml';\n"
 				append_to_file "app/assets/stylesheets/yaml_modules_and_settings.scss", File.read(yaml_layouts)
-				insert_into_file "app/assets/stylesheets/application#{detect_css_format[0]}", "#{detect_css_format[1]} require yaml_modules_and_settings\n", :after => "require_self\n"
+				append_to_file "app/assets/stylesheets/application.css.scss", "@import yaml_modules_and_settings;\n"
 			end
 
 			def detect_js_format
@@ -23,14 +23,6 @@ module Yaml
 				return ['.js.coffee', '#='] if File.exists?('app/assets/javascripts/application.js.coffee')
 				return ['.js', '//='] if File.exists?('app/assets/javascripts/application.js')
 			end
-
-			def detect_css_format
-        return ['.css', ' *='] if File.exist?('app/assets/stylesheets/application.css')
-        return ['.css.sass', ' //='] if File.exist?('app/assets/stylesheets/application.css.sass')
-        return ['.sass', ' //='] if File.exist?('app/assets/stylesheets/application.sass')
-        return ['.css.scss', ' //='] if File.exist?('app/assets/stylesheets/application.css.scss')
-        return ['.scss', ' //='] if File.exist?('app/assets/stylesheets/application.scss')
-      end
 
 			def create_layout
 				template "application.html.erb", "app/views/layouts/application.html.erb"
