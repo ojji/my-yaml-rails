265f7524e3e61d8b24a5e87e7a8da0c85ebac712
Zoltán Tóth <zshun86@gmail.com>
2013-08-04 01:01:52 +0200

    Ensure precompiling of the vendor plugins in production environment.

--- lib/yaml/engine.rb
+++ lib/yaml/engine.rb
@@ -4,6 +4,8 @@ module Yaml
 		initializer	'yaml.update_asset_paths' do |app|
 			app.config.assets.paths << File.expand_path("../../../scss", __FILE__) 
 			app.config.assets.paths << File.expand_path("../../../js", __FILE__) 
+
+			app.config.assets.precompile += %w(yaml-focusfix.js html5shiv.js html5shiv-printshiv.js)
 		end
 	end
 end
