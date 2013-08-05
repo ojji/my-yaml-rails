e58c174f82fc2d7a1a82072eda3af7d76c63b6f1
Zoltán Tóth <zshun86@gmail.com>
2013-08-05 13:16:37 +0200

    Engine.rb updated with second initializer.

--- lib/yaml/engine.rb
+++ lib/yaml/engine.rb
@@ -1,11 +1,14 @@
 module Yaml
 	class Engine < Rails::Engine
 
-		initializer	'yaml.update_asset_paths' do |app|
+		add_paths_block = lambda { |app|
 			app.config.assets.paths << File.expand_path("../../../scss", __FILE__) 
 			app.config.assets.paths << File.expand_path("../../../js", __FILE__) 
 
 			app.config.assets.precompile += %w(yaml-focusfix.js html5shiv.js html5shiv-printshiv.js)
-		end
+		}
+
+		initializer	'yaml.update_asset_paths', &add_paths_block
+		initializer	'yaml.update_asset_paths', :group => :assets, &add_paths_block
 	end
 end
